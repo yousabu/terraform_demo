d9cbc41fdfd9fa12943ced7529f7d92422445fa7
Youssef <shbanmhmdywsf8@gmail.com>
2023-03-09 12:55:42 +0200

    route table

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "aws" {
     region = "us-east-1"
+
 } 
 
 
@@ -29,3 +30,54 @@ resource "aws_subnet" "myapp-subnet1" {
 
 }
 
+
+
+resource "aws_internet_gateway" "myapp_igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+      tags = {
+    "Name" = "${var.env_prefix}-rtb"
+  }
+}
+
+###### To Create New One #######
+# resource "aws_route_table" "myapp-route-table" {
+#     vpc_id = aws_vpc.myapp-vpc.id
+
+#     route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.myapp_igw.id
+#     }
+#     tags = {
+#       "Name" = "${var.env_prefix}-rtb"
+#     }
+# }
+
+# resource "aws_route_table_association" "a-rtb-subnet" {
+#     subnet_id = aws_subnet.myapp-subnet1.id
+#     route_table_id = aws_route_table.myapp-route-table.id
+  
+# }
+
+resource "aws_default_route_table" "main_route_table" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp_igw.id
+    }
+    tags = {
+      "Name" = "${var.env_prefix}-main-rtb"
+  }
+}
+
+
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = [var.my_ip]
+    }
+}
